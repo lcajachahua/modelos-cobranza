8d04a29e131f9614f54b5d0e841dcc2780c9b111
Luis C <lcajachahua@gmail.com>
2022-07-13 13:05:36 -0500

    Update training.py

--- model_definitions/a957f9c6-a96e-4f06-a141-a63a699632e8/model_modules/training.py
+++ model_definitions/a957f9c6-a96e-4f06-a141-a63a699632e8/model_modules/training.py
@@ -22,7 +22,7 @@ def train(data_conf, model_conf, **kwargs):
     target_name = "TAR"
 
     # read training dataset from Teradata and convert to pandas
-    train_df = DataFrame(data_conf["table"])
+    train_df = DataFrame("ACC_TRAIN")
     train_df = train_df.select([feature_names + [target_name]])
     train_df = train_df.to_pandas()
 
